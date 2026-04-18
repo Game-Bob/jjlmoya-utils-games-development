cadae5850e9befc8105c0250e42e6284ad18d5d9
jjlmoya <juanjo1989@gmail.com>
2026-04-18 21:28:55 +0200

    fix: remove re-exports of removed component vars from main index

--- src/index.ts
+++ src/index.ts
@@ -17,39 +17,15 @@ export type {
 
 export { ALL_ENTRIES, ALL_TOOLS } from './tools';
 
-export {
-  ScoreKeeperComponent,
-  ScoreKeeperSEO,
-  ScoreKeeperBibliography,
-  SCORE_KEEPER_TOOL,
-  scoreKeeper,
-} from './tool/scoreKeeper/index';
+export { SCORE_KEEPER_TOOL, scoreKeeper } from './tool/scoreKeeper/index';
 export type { ScoreKeeperUI, ScoreKeeperLocaleContent } from './tool/scoreKeeper/index';
 
-export {
-  TournamentBracketComponent,
-  TournamentBracketSEO,
-  TournamentBracketBibliography,
-  TOURNAMENT_BRACKET_TOOL,
-  tournamentBracket,
-} from './tool/tournamentBracket/index';
+export { TOURNAMENT_BRACKET_TOOL, tournamentBracket } from './tool/tournamentBracket/index';
 export type { TournamentBracketUI, TournamentBracketLocaleContent } from './tool/tournamentBracket/index';
 
-export {
-  GymTrackerComponent,
-  GymTrackerSEO,
-  GymTrackerBibliography,
-  GYM_TRACKER_TOOL,
-  gymTracker,
-} from './tool/gymTracker/index';
+export { GYM_TRACKER_TOOL, gymTracker } from './tool/gymTracker/index';
 export type { GymTrackerUI, GymTrackerLocaleContent } from './tool/gymTracker/index';
 
-export {
-  ReactionTesterComponent,
-  ReactionTesterSEO,
-  ReactionTesterBibliography,
-  REACTION_TESTER_TOOL,
-  reactionTester,
-} from './tool/reactionTester/index';
+export { REACTION_TESTER_TOOL, reactionTester } from './tool/reactionTester/index';
 export type { ReactionTesterUI, ReactionTesterLocaleContent } from './tool/reactionTester/index';
 
