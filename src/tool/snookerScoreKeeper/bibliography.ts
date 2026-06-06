ce04aa18344791ffa85bb837c78d44b70d5be49c
jjlmoya <juanjo1989@gmail.com>
2026-06-06 16:00:26 +0200

    fix: bibliography

--- src/tool/snookerScoreKeeper/bibliography.ts
+++ src/tool/snookerScoreKeeper/bibliography.ts
@@ -2,9 +2,5 @@ export const bibliography = [
   {
     name: 'WPBSA Official Snooker Rules',
     url: 'https://wpbsa.com/rules/',
-  },
-  {
-    name: 'World Snooker Tour Rules and Regulations',
-    url: 'https://wst.tv/rules/',
-  },
+  }
 ];
